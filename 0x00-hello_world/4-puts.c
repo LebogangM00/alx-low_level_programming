6b8b2fe90ae12b5b801f989837c66a136a466820
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-16 04:30:03 -0700

    vi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle\n");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
 
