6db7722c266bbe99148c200c9f0c2d283f099d8f
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-20 14:21:29 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,44 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_times_table - Prints the n times table up to 13.
- * @n: The number of times.
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number of times to print the table
  */
 void print_times_table(int n)
 {
-    int i, j;
+	if (n > 15 || n < 0)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= 13; j++)
-        {
-            printf("%-4d", i * j);
-        }
-        printf("\n");
-    }
+	int i, j, result;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			if (j != 0)
+				_putchar(',');
+
+			if (result < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (result >= 10 && result < 100)
+			{
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+			}
+			else if (result >= 100)
+			{
+				_putchar((result / 100) + '0');
+				_putchar(((result / 10) % 10) + '0');
+			}
+
+			_putchar((result % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
 }
 
