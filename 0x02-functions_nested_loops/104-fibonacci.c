aa4478124753a9642cfedc5d7931a0b1e1ea24e7
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-20 13:46:09 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,23 +2,26 @@
 /**
  * main - Entry point
  *
- * Return: 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-        int count;
-        unsigned long fib1 = 1, fib2 = 2, nextFib;
+	int count;
+	unsigned int fib1 = 1, fib2 = 2, nextFib;
 
-        printf("%lu, %lu", fib1, fib2);
-        for (count = 3; count <= 98; count++)
-        {
-                nextFib = fib1 + fib2;
-                printf(", %lu", nextFib);
-                fib1 = fib2;
-                fib2 = nextFib;
-        }
-        printf("\n");
+	printf("%u, %u", fib1, fib2);
 
-        return (0);
+	for (count = 3; count <= 98; count++)
+	{
+		nextFib = fib1 + fib2;
+		printf(", %u", nextFib);
+
+		fib1 = fib2;
+		fib2 = nextFib;
+	}
+
+	printf("\n");
+
+	return (0);
 }
 
