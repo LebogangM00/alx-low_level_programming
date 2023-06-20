44781f43b1233db8fa1f6b5e990819c877b1034c
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-20 12:53:43 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,8 +2,6 @@
 /**
  * main - Entry point
  *
- * Description: Prints the first 98 Fibonacci numbers
- *
  * Return: 0 (Success)
  */
 int main(void)
@@ -11,11 +9,11 @@ int main(void)
         int count;
         unsigned long fib1 = 1, fib2 = 2, nextFib;
 
-        printf("%lu", fib1);
-        for (count = 1; count < 98; count++)
+        printf("%lu, %lu", fib1, fib2);
+        for (count = 3; count <= 98; count++)
         {
-                printf(", %lu", fib2);
                 nextFib = fib1 + fib2;
+                printf(", %lu", nextFib);
                 fib1 = fib2;
                 fib2 = nextFib;
         }
