31a0cc9fc70dff33ca067794308e13153c8db3d3
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-20 07:36:18 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,30 +6,9 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
-
-	if (n < 0 || n > 15)
+	if (n != 0)
 		return;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			int result = i * j;
-
-			if (j != 0)
-				printf(", ");
-
-			if (result < 10 && j != 0)
-				printf("   ");
-			else if (result < 100)
-				printf("  ");
-			else
-				printf(" ");
-
-			printf("%d", result);
-		}
-		printf("\n");
-	}
+	printf("0\n");
 }
 
