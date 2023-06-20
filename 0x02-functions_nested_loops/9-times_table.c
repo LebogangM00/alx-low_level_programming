b6ee105452d7a2562210659327dc443768cf5375
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-20 07:05:42 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,18 +8,17 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (column = 0; column <= 9; column++)
+		_putchar('0'); /* Always print the first digit of the row */
+
+		for (column = 1; column <= 9; column++)
 		{
 			result = row * column;
 
-			if (column != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(',');
+			_putchar(' ');
 
-			if (result < 10 && column != 0)
-				_putchar(' ');
+			if (result < 10)
+				_putchar(' '); /* Extra space for single-digit numbers */
 
 			_putchar(result % 10 + '0');
 		}
