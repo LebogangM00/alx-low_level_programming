037382c462e43a129ad31c128adb793d0b325807
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-26 04:39:15 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * print_rev - Prints a string in reverse, followed by a new line.
- * @s: Pointer to the string to be printed in reverse.
+ * print_rev - Prints a string in reverse, followed by a new line
+ * @s: The string to be printed in reverse
  */
 void print_rev(char *s)
 {
+int length = 0;
 int i;
 
-i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
+/* Calculate the length of the string */
+while (s[length] != '\0')
+length++;
 
-while (i >= 0)
-{
+/* Print the string in reverse */
+for (i = length - 1; i >= 0; i--)
 _putchar(s[i]);
-i--;
-}
+
 _putchar('\n');
 }
 
