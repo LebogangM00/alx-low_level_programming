007ae77c4a76a275d7b6490f1c81e7afdc2c9404
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-26 12:54:06 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,22 +9,23 @@
  */
 int main(void)
 {
-    int pass[100];
-    int i, sum, n;
+    int password[100];
+    int i, sum, random_char;
 
     sum = 0;
     srand(time(NULL));
 
     for (i = 0; i < 100; i++)
     {
-        pass[i] = rand() % 78;
-        sum += pass[i];
-        putchar(pass[i] + 33);
-        if (sum >= 2772 - 128)
+        random_char = rand() % 94 + 33;
+        password[i] = random_char;
+        sum += random_char;
+        putchar(random_char);
+        if (sum >= 2772 - 126)
         {
-            n = 2772 - sum;
-            sum += n;
-            putchar(n + 33);
+            random_char = 2772 - sum;
+            sum += random_char;
+            putchar(random_char);
             break;
         }
     }
