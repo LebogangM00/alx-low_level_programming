8cbd549c9c265e4af945023f6bce2f22c25af616
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-06-28 15:17:02 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,54 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * infinite_add - Adds two numbers
- * @n1: First number as a string
- * @n2: Second number as a string
- * @r: Buffer to store the result
- * @size_r: Size of the buffer
+ * @n1: The first number
+ * @n2: The second number
+ * @r: The buffer to store the result
+ * @size_r: The size of the buffer
  *
- * Return: Pointer to the result
+ * Return: Pointer to the result (r) or 0 if result cannot be stored
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1, len2, i, j, sum, carry;
+int len1 = 0, len2 = 0, len, carry, sum;
+int i, j;
 
-len1 = 0;
 while (n1[len1] != '\0')
 len1++;
-
-len2 = 0;
 while (n2[len2] != '\0')
 len2++;
 
-if (size_r <= len1 || size_r <= len2)
+if (len1 >= size_r || len2 >= size_r)
 return (0);
 
+len = len1 >= len2 ? len1 : len2;
 carry = 0;
-i = len1 - 1;
-j = len2 - 1;
-r[size_r - 1] = '\0';
 
-while (i >= 0 || j >= 0 || carry != 0)
+for (i = len1 - 1, j = len2 - 1; len >= 0; i--, j--, len--)
 {
 sum = carry;
+
 if (i >= 0)
 sum += n1[i] - '0';
 if (j >= 0)
 sum += n2[j] - '0';
 
 carry = sum / 10;
-r[size_r - 2] = sum % 10 + '0';
-
-i--;
-j--;
-size_r--;
+r[len] = sum % 10 + '0';
 }
 
-return (r + size_r - 1);
+if (carry)
+{
+if (len >= 0)
+r[len] += carry;
+else
+return (0);
 }
 
+if (len < 0 && carry)
+return (0);
+
+return (r);
+}
