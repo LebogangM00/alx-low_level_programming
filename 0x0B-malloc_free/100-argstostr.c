dccc9b4a259f77bdf36dfa3c0136e4c1de1bee7b
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-07-10 00:15:13 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of a program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: pointer to the concatenated string, or NULL on failure
+ */
+char *argstostr(int ac, char **av)
+{
+char *concatenated;
+int totalLength = 0, i, k;
+int argLength;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+/* Calculate the total length of the arguments plus newlines */
+for (i = 0; i < ac; i++)
+{
+argLength = 0;
+while (av[i][argLength] != '\0')
+argLength++;
+totalLength += argLength + 1; /* Add 1 for newline */
+}
+
+concatenated = malloc((totalLength + 1) * sizeof(char)); /* Add 1 for null-terminator */
+if (concatenated == NULL)
+return (NULL);
+
+k = 0;
+for (i = 0; i < ac; i++)
+{
+argLength = 0;
+while (av[i][argLength] != '\0')
+{
+concatenated[k] = av[i][argLength];
+k++;
+argLength++;
+}
+concatenated[k] = '\n'; /* Add newline character */
+k++;
+}
+concatenated[k] = '\0'; /* Add null-terminator */
+
+return (concatenated);
+}
+
