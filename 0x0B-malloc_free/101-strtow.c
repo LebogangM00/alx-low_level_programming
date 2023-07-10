5d2d568dea30edb07dcc71226abea86cdc4efb7d
LebogangM00 <lebogang.muthwari46@gmail.com>
2023-07-10 01:23:13 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 
 /**
+ * is_separator - Checks if a character is a separator
+ * @c: The character to check
+ *
+ * Return: 1 if the character is a separator, 0 otherwise
+ */
+int is_separator(char c)
+{
+char separators[] = " \t\n.,;:!\"(){}[]<>";
+int i;
+
+for (i = 0; separators[i] != '\0'; i++)
+{
+if (c == separators[i])
+return (1);
+}
+
+return (0);
+}
+
+/**
  * count_words - Counts the number of words in a string
  * @str: The input string
  *
@@ -9,16 +29,16 @@
  */
 int count_words(char *str)
 {
-int count = 0, i, is_word = 0;
+int count = 0, is_word = 0;
 
-for (i = 0; str[i] != '\0'; i++)
+for (int i = 0; str[i] != '\0'; i++)
 {
-if (str[i] != ' ' && !is_word)
+if (!is_separator(str[i]) && !is_word)
 {
 is_word = 1;
 count++;
 }
-else if (str[i] == ' ')
+else if (is_separator(str[i]))
 {
 is_word = 0;
 }
@@ -37,20 +57,19 @@ return (count);
 char **allocate_words(char *str, int word_count)
 {
 char **words;
-int i;
 
 words = malloc((word_count + 1) * sizeof(char *));
 if (words == NULL)
 return (NULL);
 
+int i, len, j = 0, k = 0;
 for (i = 0; i < word_count; i++)
 {
-int len = 0, j = 0;
-
-while (str[j] == ' ')
+while (is_separator(str[j]))
 j++;
 
-while (str[j + len] != ' ' && str[j + len] != '\0')
+len = 0;
+while (!is_separator(str[j + len]) && str[j + len] != '\0')
 len++;
 
 words[i] = malloc((len + 1) * sizeof(char));
@@ -62,11 +81,11 @@ free(words);
 return (NULL);
 }
 
-for (j = 0; j < len; j++)
-words[i][j] = str[j];
+for (k = 0; k < len; k++)
+words[i][k] = str[j + k];
 
-words[i][len] = '\0';
-str += len;
+words[i][k] = '\0';
+j += len;
 }
 
 words[word_count] = NULL;
